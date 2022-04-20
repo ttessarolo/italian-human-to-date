eb5ad53f262ddb1eb6ba513b32e94a8fb9ec83d7
Tommaso Tessarolo <tommaso@tessarolo.it>
2022-04-20 11:53:45 +0200

    feat: object is now a class and works with streams, callback and events

--- index.js
+++ index.js
@@ -1,86 +1,126 @@
 'use strict';
 
 import natural from 'natural';
-
+import Events from 'events';
 import extractors from './extractors/index.js';
 import adjust from './lib/adjust.js';
 import { getTZDate } from './lib/dateUtils.js';
 import { italianDateTerms, extractorSequence } from './defaults.js';
 
-export default function extractDates(
-  data,
-  {
-    now,
-    timeZone,
-    sequence,
-    vocabulary,
-    textProcessor = {
-      stemmer: 'PorterStemmerIt',
-      stopwords: true,
-      duplicate: false,
-      diacritics: true,
-      punctuation: true,
-      lowercase: true,
-      trim: true,
-      tokenize: true,
-    },
-    stripChar = '≈',
-    verbose = false,
-  } = {
-    vocabulary: italianDateTerms,
-    stripChar: '≈',
-    verbose: false,
-    textProcessor: {
-      stemmer: 'PorterStemmerIt',
-      stopwords: true,
-      duplicate: false,
-      diacritics: true,
-      punctuation: true,
-      lowercase: true,
-      trim: true,
-      tokenize: true,
-    },
+export default class DateExtractor extends Events {
+  constructor(
+    {
+      now,
+      timeZone,
+      sequence,
+      vocabulary,
+      textProcessor = {
+        stemmer: 'PorterStemmerIt',
+        stopwords: true,
+        duplicate: false,
+        diacritics: true,
+        punctuation: true,
+        lowercase: true,
+        trim: true,
+        tokenize: true,
+      },
+      stripChar = '≈',
+      verbose = false,
+    } = {
+      vocabulary: italianDateTerms,
+      stripChar: '≈',
+      verbose: false,
+      textProcessor: {
+        stemmer: 'PorterStemmerIt',
+        stopwords: true,
+        duplicate: false,
+        diacritics: true,
+        punctuation: true,
+        lowercase: true,
+        trim: true,
+        tokenize: true,
+      },
+    }
+  ) {
+    super();
+
+    this.vocabolario = {
+      prossimo: vocabulary.next.map((k) => natural[textProcessor.stemmer].stem(k)),
+      scorso: vocabulary.previous.map((k) => natural[textProcessor.stemmer].stem(k)),
+      ieri: vocabulary.yesterday.map((k) => natural[textProcessor.stemmer].stem(k)),
+      oggi: vocabulary.today.map((k) => natural[textProcessor.stemmer].stem(k)),
+      domani: vocabulary.tomorrow.map((k) => natural[textProcessor.stemmer].stem(k)),
+      settimana: vocabulary.week.map((k) => natural[textProcessor.stemmer].stem(k)),
+      mese: vocabulary.month.map((k) => natural[textProcessor.stemmer].stem(k)),
+      mesi: vocabulary.months.map((k) => natural[textProcessor.stemmer].stem(k)),
+      giorniSettimana: vocabulary.weekDays.map((k) => natural[textProcessor.stemmer].stem(k)),
+      anno: vocabulary.year.map((k) => natural[textProcessor.stemmer].stem(k)),
+      dopo: vocabulary.after.map((k) => natural[textProcessor.stemmer].stem(k)),
+      prima: vocabulary.before.map((k) => natural[textProcessor.stemmer].stem(k)),
+      dopoDomani: vocabulary.afterTomorrow.map((k) => natural[textProcessor.stemmer].stem(k)),
+      altroIeri: vocabulary.beforeYesterday.map((k) => natural[textProcessor.stemmer].stem(k)),
+      traFra: vocabulary.within.map((k) => natural[textProcessor.stemmer].stem(k)),
+      fa: vocabulary.ago.map((k) => natural[textProcessor.stemmer].stem(k)),
+      giorno: vocabulary.day.map((k) => natural[textProcessor.stemmer].stem(k)),
+      ultimo: vocabulary.last.map((k) => natural[textProcessor.stemmer].stem(k)),
+    };
+
+    textProcessor.numeri = vocabulary.numbers.map((k) => natural[textProcessor.stemmer].stem(k));
+
+    this.textProcessor = textProcessor;
+    this.now = getTZDate(now, timeZone);
+    this.sequence = sequence;
+    this.stripChar = stripChar;
+    this.verbose = verbose;
   }
-) {
-  const vocabolario = {
-    prossimo: vocabulary.next.map((k) => natural[textProcessor.stemmer].stem(k)),
-    scorso: vocabulary.previous.map((k) => natural[textProcessor.stemmer].stem(k)),
-    ieri: vocabulary.yesterday.map((k) => natural[textProcessor.stemmer].stem(k)),
-    oggi: vocabulary.today.map((k) => natural[textProcessor.stemmer].stem(k)),
-    domani: vocabulary.tomorrow.map((k) => natural[textProcessor.stemmer].stem(k)),
-    settimana: vocabulary.week.map((k) => natural[textProcessor.stemmer].stem(k)),
-    mese: vocabulary.month.map((k) => natural[textProcessor.stemmer].stem(k)),
-    mesi: vocabulary.months.map((k) => natural[textProcessor.stemmer].stem(k)),
-    giorniSettimana: vocabulary.weekDays.map((k) => natural[textProcessor.stemmer].stem(k)),
-    anno: vocabulary.year.map((k) => natural[textProcessor.stemmer].stem(k)),
-    dopo: vocabulary.after.map((k) => natural[textProcessor.stemmer].stem(k)),
-    prima: vocabulary.before.map((k) => natural[textProcessor.stemmer].stem(k)),
-    dopoDomani: vocabulary.afterTomorrow.map((k) => natural[textProcessor.stemmer].stem(k)),
-    altroIeri: vocabulary.beforeYesterday.map((k) => natural[textProcessor.stemmer].stem(k)),
-    traFra: vocabulary.within.map((k) => natural[textProcessor.stemmer].stem(k)),
-    fa: vocabulary.ago.map((k) => natural[textProcessor.stemmer].stem(k)),
-    giorno: vocabulary.day.map((k) => natural[textProcessor.stemmer].stem(k)),
-    ultimo: vocabulary.last.map((k) => natural[textProcessor.stemmer].stem(k)),
-  };
 
-  textProcessor.numeri = vocabulary.numbers.map((k) => natural[textProcessor.stemmer].stem(k));
+  async extract(ingress, { now, timeZone, cb, outStream } = {}) {
+    const results = [];
+    const today = now ? getTZDate(now, timeZone) : this.now;
+    const isAsync = cb || outStream;
+    const [rows, isMultiple] = (() => {
+      if (Array.isArray(ingress)) return [ingress, true];
+      return [[ingress], false];
+    })();
 
-  const tokens = adjust(data, textProcessor);
-  const today = getTZDate(now, timeZone);
-  const dates = new Map();
-  const ranges = new Map();
+    for await (const data of rows) {
+      const dates = new Map();
+      const ranges = new Map();
+      const tokens = adjust(data, this.textProcessor);
+      const adjustedTokes = [...tokens];
 
-  (sequence ?? extractorSequence).forEach((seq) =>
-    extractors[seq]({ tokens, today, dates, ranges, stripChar, vocabolario, verbose })
-  );
+      (this.sequence ?? extractorSequence).forEach((seq) =>
+        extractors[seq]({
+          tokens,
+          today,
+          dates,
+          ranges,
+          stripChar: this.stripChar,
+          vocabolario: this.vocabolario,
+          verbose: this.verbose,
+        })
+      );
 
-  const date = [...dates.values()].sort();
+      const date = [...dates.values()].sort();
 
-  if (verbose) console.log(date.map((d) => d.toString()));
+      if (this.verbose) console.log(date.map((d) => d.toString()));
 
-  return {
-    dates: date,
-    ranges: [...ranges.values()],
-    tokens,
-  };
+      const ret = {
+        origin: data,
+        dates: date,
+        ranges: [...ranges.values()],
+        adjustedTokes,
+        residualTokens: tokens.filter((k) => !(k === this.stripChar)),
+        strips: tokens,
+      };
+
+      if (cb) cb(null, ret);
+      if (outStream) outStream.write(ret);
+      this.emit('data', ret);
+
+      if (!isAsync) results.push(ret);
+    }
+
+    return isMultiple ? results : results[0];
+  }
 }
